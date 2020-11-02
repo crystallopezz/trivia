223e5fb52bff44de70e49652a4f028d49bf4e68f
Crystal Lopez <crystallopez289@gmail.com>
2020-11-02 03:29:09 +0000

    randomly choose qs for round, ask one q at a time, answer order randomized, ability to answer, correct answer revealed after guess submitted, print score at end

--- trivia.py
+++ trivia.py
@@ -32,28 +32,53 @@ for q in question_data:
         "correct": q["correct"]
     }
 
-print(questions)
-
 #convert keys to list
-qs_avail = questions.keys()
+qs_avail = list(questions.keys())
+random.shuffle(qs_avail)
+round_qs = qs_avail[0:10]
 
-questions_asked = 0
+score = 0
 
-#while questions_asked < total_questions (10): 
-# while questions_asked < 10: 
-#     #randomly choose q from list of keys
-#     curr_question = choice(qs_avail)
-    #combine correct and incorrect answers and randomly generate answer order
+for curr_question in round_qs: 
+    possible_ans = questions[curr_question]["possibilities"]
+    # randomly generate answer order
+    random.shuffle(possible_ans)
+    print(f"{questions[curr_question]['question']}")
+
+    
+    for i in range(0, len(possible_ans)): 
+        print(f"{i+1}: {possible_ans[i]}")
+
+    guess = int(input("Please choose one of the above options by writing the integer of the answer you choose."))
+    
     #when user answers: 
         #make sure input is valid
         #compare the answer with correct answer from dict
+    if questions[curr_question]["correct"] == possible_ans[guess-1]: 
         #if answer correct: 
             #plus 1 to score
+        score += 1
             #inform user answer is correct
+        print(f"{questions[curr_question]['correct']} is correct!")
         #else: 
+    else: 
             #inform user answer incorrect, display correct ans
-        #plus one to questions_asked
-        #remove question_id from list of qs_avail
-#when questions_asked == total_questions: 
-    #display score
-    #display some nice message
+        print(f"Sorry, the correct answer was {questions[curr_question]['correct']}")
+
+print(score)
+
+    
+
+#code from when i was going to increment qs_asked until 10 and randomly choose a question each time
+#while questions_asked < total_questions (10): 
+# while questions_asked < 10: 
+#     #randomly choose q from list of keys
+#     curr_question = random.choice(qs_avail)
+#     qs_avail.remove(curr_question)
+#     possible_ans = questions[curr_question]["possibilities"]
+#     #randomly generate answer order
+#     random.shuffle(possible_ans)
+#     for i in range(0, len(possible_ans)): 
+#         print(f"{i+1}: {possible_ans[i]}")
+
+#     questions_asked += 1
